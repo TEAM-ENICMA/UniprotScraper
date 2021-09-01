5178b9c67cda116fc92af12170484741192028e7
Ozgur Can Arıcan <56658730+ozgurcanarican@users.noreply.github.com>
2021-09-01 14:36:47 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,16 @@ import setuptools
 setup(
   name = 'UniprotScraper',         
   packages = ['UniprotScraper'],   
-  version = '0.1',     
+  version = '0.2',     
   license='MIT',        
   description = 'UniprotScraper is a web scraper tool that specialized for Uniprot Database.',
   author = 'team_enicma',                  
   author_email = 'teamenicma@gmail.com',      
   url = 'https://github.com/TEAM-ENICMA/UniprotScraper',
-  download_url = 'https://github.com/TEAM-ENICMA/UniprotScraper/archive/refs/tags/v_1.0.tar.gz',
+  download_url = 'https://github.com/TEAM-ENICMA/UniprotScraper/archive/refs/tags/v0.2.tar.gz',
   keywords = ['Python', 'Bioinformatics', 'WebScraping', 'ComputationalBiology', 'Uniprot'],
   install_requires=[
-          'bs4',
-          'urllib.request',
-          'urllib.error'
+          'requests'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      
