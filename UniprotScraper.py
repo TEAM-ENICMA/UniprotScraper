34614f34a804e84b5af925bf379b522fbf768dc6
Ozgur Can Arıcan <56658730+ozgurcanarican@users.noreply.github.com>
2021-09-01 14:20:14 +0300

    Update UniprotScraper.py

--- UniprotScraper.py
+++ UniprotScraper.py
@@ -1,23 +1,23 @@
-import bs4
-import urllib.request
-from urllib.error import HTTPError, URLError
+import requests
 
 #__co-authors__: Team_Enicma; Bilgehan Nevruz, Melih Temel, Umut Durak, Özgür Can Arıcan
 #__contact__: www.github.com/TEAM-ENICMA, teamenicma@gmail.com
-#__version__: UniprotScraper v_1.0
+#__version__: UniprotScraper 0.2
 
 def __scraping_uniprot(UniEntryNumber):
     link = "https://www.uniprot.org/uniprot/" + UniEntryNumber + ".fasta"
     try:
-        url = urllib.request.urlopen(link).read()
-    except HTTPError:
-        text = "HTTP_ERROR: Page not found!\nPlease check your 'Uniprot Entry Number'"
-    except URLError:
-        text = "URL_ERROR: No network connection!\n Please check your internet connection"
+        url = requests.get(link)
+    except:
+        text = "ERROR: No network connection! Please check your internet connection"
+        return text
     else:
-        scrape = bs4.BeautifulSoup(url, "lxml")
-        text = scrape.get_text()
-    return text
+        text = url.text
+        if text[:3] == ">sp":
+            return text
+        else:
+            text = "ERROR: Page not found! Please check your 'Uniprot Entry Number"
+            return text
 
 def get_with_header(UniEntryNumber):
     content = __scraping_uniprot(UniEntryNumber)
@@ -25,30 +25,41 @@ def get_with_header(UniEntryNumber):
 
 def get_without_header(UniEntryNumber):
     content = __scraping_uniprot(UniEntryNumber)
-    if content[:4] == "HTTP" or content[:4] == "URL_":
+    if content[:5] == "ERROR":
         return content
     return "\n".join(content.splitlines()[1:])
 
 def get_only_header(UniEntryNumber):
     content = __scraping_uniprot(UniEntryNumber)
-    if content[:4] == "HTTP" or content[:4] == "URL_":
+    if content[:5] == "ERROR":
         return content
     return content.splitlines()[0]
 
 def get_first_three_aa(UniEntryNumber):
     content = __scraping_uniprot(UniEntryNumber)
-    if content[:4] == "HTTP" or content[:4] == "URL_":
+    if content[:5] == "ERROR":
         return content
     return content.splitlines()[1][:3]
 
 def get_first_five_aa(UniEntryNumber):
     content = __scraping_uniprot(UniEntryNumber)
-    if content[:4] == "HTTP" or content[:4] == "URL_":
+    if content[:5] == "ERROR":
         return content
     return content.splitlines()[1][:5]
 
 def get_first_ten_aa(UniEntryNumber):
     content = __scraping_uniprot(UniEntryNumber)
-    if content[:4] == "HTTP" or content[:4] == "URL_":
+    if content[:5] == "ERROR":
         return content
     return content.splitlines()[1][:10]
+
+def get_aa_content(UniprotEntryNumber):
+    content = __scraping_uniprot(UniprotEntryNumber)
+    if content[:5] == "ERROR":
+        return content
+    else:
+        sequence = "\n".join(content.splitlines()[1:])
+        aa_dict = dict()
+        for a in sequence:
+            aa_dict[a] = aa_dict.get(a, 0) + 1
+        return aa_dict
