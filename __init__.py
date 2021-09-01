b40acc25b527f81ee5aac670243674058faf6647
Ozgur Can Arıcan <56658730+ozgurcanarican@users.noreply.github.com>
2021-09-01 14:21:35 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-from .UniprotScraper import get_with_header, get_without_header, get_only_header, get_first_three_aa, get_first_five_aa, get_first_ten_aa
+from .UniprotScraper import get_with_header, get_without_header, get_only_header, get_first_three_aa, get_first_five_aa, get_first_ten_aa, get_aa_content
